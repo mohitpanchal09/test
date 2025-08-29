638169f28ed907a302ba0908b32a140e2af2336b
mohitpanchal09 <panchalmohitg2002@gmail.com>
2025-08-30 00:36:10 +0530

    chng

--- src/components/custom/CustomizedDiets/NonVegSection.tsx
+++ src/components/custom/CustomizedDiets/NonVegSection.tsx
@@ -23,7 +23,9 @@ export default function VegNonVegSection() {
           <div className="flex justify-between items-center w-full mb-6 sm:mb-10">
             <div className="flex-1 text-white text-xl sm:text-3xl font-bold h-16 flex items-center justify-between bg-white/10 rounded-lg  border-[0.5px] border-white mr-20">
               <Button
-                className={`cursor-pointer text-lg min-w-[200px] text-center text-white mx-auto h-12 w-fit p-2 rounded-lg  flex items-center ${selected=="veg" ? "border":""}`}
+                className={`cursor-pointer text-lg min-w-[200px] text-center text-white mx-auto h-12 w-fit p-2 rounded-lg  flex items-center ${
+                  selected == "veg" ? "border" : ""
+                }`}
                 style={{
                   background: "rgba(255, 255, 255, 0.10)",
                 }}
@@ -32,7 +34,9 @@ export default function VegNonVegSection() {
                 Veg
               </Button>
               <Button
-                className={`flex-1 cursor-pointer text-lg max-w-[200px] text-center text-white mx-auto h-12 w-fit p-2 rounded-lg  flex items-center ${selected=="non-veg" ? "border":""}`}
+                className={`flex-1 cursor-pointer text-lg max-w-[200px] text-center text-white mx-auto h-12 w-fit p-2 rounded-lg  flex items-center ${
+                  selected == "non-veg" ? "border" : ""
+                }`}
                 style={{
                   background: "rgba(255, 255, 255, 0.10)",
                 }}
@@ -53,6 +57,30 @@ export default function VegNonVegSection() {
 
       {/* Mobile/Tablet */}
       <div className="lg:hidden w-[95%] bg-gradient-to-b from-[#B3FF00]/20 via-[#009245]/20 to-[#034824]/20 rounded-3xl border border-white/20 backdrop-blur-md p-4 space-y-4">
+        <div className=" text-white gap-x-2 p-2 text-xl sm:text-3xl font-bold h-16 flex items-center justify-between bg-white/10 rounded-lg   border-white w-full">
+          <Button
+            className={`flex-1/2 cursor-pointer text-lg  text-center text-white mx-auto h-12 w-fit p-2 rounded-lg  flex items-center ${
+              selected == "veg" ? "border" : ""
+            }`}
+            style={{
+              background: "rgba(255, 255, 255, 0.10)",
+            }}
+            onClick={() => setSelected("veg")}
+          >
+            Veg
+          </Button>
+          <Button
+            className={`flex-1/2 cursor-pointer text-lg  text-center text-white mx-auto h-12 w-fit p-2 rounded-lg  flex items-center ${
+              selected == "non-veg" ? "border" : ""
+            }`}
+            style={{
+              background: "rgba(255, 255, 255, 0.10)",
+            }}
+            onClick={() => setSelected("non-veg")}
+          >
+            Non-Veg
+          </Button>
+        </div>
         {cards.map((card, idx) => (
           <DietCard key={idx} {...card} />
         ))}
